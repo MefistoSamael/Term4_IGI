c59efc5f0a03b55743019a6896a9b42a7f0f5605
Vassyan <vasa060504@mail.ru>
2023-02-21 19:40:54 +0300

    STEP 4 task 2 done

--- Lab1/main.py
+++ Lab1/main.py
@@ -5,3 +5,21 @@ def task1():
     print(MyConstants.greetings_message)
 
 
+def task2(number1, number2, operation):
+    if operation == "add":
+        return number1 + number2
+    elif operation == "sub":
+        return number1 - number2
+    elif operation == "mult":
+        return number1 * number2
+    elif operation == "div":
+        if number2:
+            return number1 / number2
+        else:
+            return "Error: division by zero"
+    else:
+        return "Error: invalid operation"
+
+
+task1()
+print(task2(5, 0, "div"))
